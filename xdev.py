88f06ee2df35668a1d9405b7b6b3cd5ff8f2be51
HarshithXDev <harshithxdev@gmail.com>
2024-09-05 21:42:06 +0530

    Create xdev.py

---
+++ xdev.py
@@ -0,0 +1,145 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telegraph import upload_file
+
+Bot = Client(
+    "Telegraph Uploader Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
+
+START_TEXT = """
+Hello {}, I am an under 5MB media or file to telegra.ph link uploader bot.
+
+Powered By »» @HarshithXDev
+"""
+HELP_TEXT = """
+- Just give me a media under 5MB
+- Then I will download it
+- I will then upload it to the telegra.ph link
+
+Powered By »» @HarshithXDev
+"""
+ABOUT_TEXT = """
+- **Bot :** `Telegraph Uploader`
+- **Creator :** [HarshithXDev](https://telegram.me/HarshithXDev)
+- **Channel :** [HarshithX](https://telegram.me/HarshithX)
+- **Source :** [Click here](https://github.com/HarshithXDev/TelegraphBot)
+- **Language :** [Python3](https://python.org)
+- **Library :** [Pyrogram](https://pyrogram.org)
+- **Server :** [Heroku](https://heroku.com)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/HarshithX'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/HarshithXDev')
+        ],[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+
+@Bot.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    else:
+        await update.message.delete()
+    
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = START_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=reply_markup
+    )
+
+@Bot.on_message(filters.private & filters.media)
+async def getmedia(bot, update):
+    medianame = DOWNLOAD_LOCATION + str(update.from_user.id)
+    try:
+        message = await update.reply(
+            text="`Processing...`",
+            quote=True,
+            disable_web_page_preview=True
+        )
+        await bot.download_media(
+            message=update,
+            file_name=medianame
+        )
+        response = upload_file(medianame)
+        try:
+            os.remove(medianame)
+        except:
+            pass
+    except Exception as error:
+        text=f"Error :- <code>{error}</code>"
+        reply_markup=InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton('More Help', callback_data='help')
+            ]]
+        )
+        await update.edit_text(
+            text=text,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
+        return
+    text=f"**Link :-** `https://telegra.ph{response[0]}`\n\n**Join :-** @HarshithX"
+    reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
+                InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+            ],
+            [
+                InlineKeyboardButton(text="Join Updates Channel", url="https://telegram.me/HarshithX")
+            ]
+        ]
+    )
+    await update.edit_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+Bot.run()
